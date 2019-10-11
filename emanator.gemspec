ab3fad720998b452a5b12c61d3b2a09ae5a61f96
Frank Murphy <fmurphy@instructure.com>
2019-10-11 13:11:05 -0400

    Add integration test

--- emanator.gemspec
+++ emanator.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency 'sql-parser', '~> 0.0.2'
   spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'sqlite3', '~> 1.4.1'
   spec.add_development_dependency 'pry-byebug', '~> 3.7.0'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- spec/emanator_spec.rb
+++ spec/emanator_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sqlite3'
+
 RSpec.describe Emanator do
   it 'has a version number' do
     expect(Emanator::VERSION).not_to be nil
@@ -31,6 +33,27 @@ RSpec.describe Emanator do
           ['INSERT INTO target VALUES (?,?)', [1, 'Backup and Restore']],
           ['INSERT INTO target VALUES (?,?)', [3, 'Replication']],
           ['DELETE FROM target WHERE (a = ?)', [1]]
+        ]
+      )
+    end
+  end
+
+  describe 'when integrated with sqlite' do
+    it '#process updates the view' do
+      db = SQLite3::Database.new ':memory:'
+      db.execute('CREATE TABLE emanator_progress (txid INTEGER);')
+      db.execute('CREATE TABLE target (a INTEGER, b STRING);')
+
+      data = File.new('spec/fixtures/cd-delete.json').read
+      change_data = Emanator::ChangeData.parse_wal2json(data)
+
+      view = Emanator::Replica.new('target', 'SELECT a, b FROM table2_with_pk')
+      view.process(change_data).each do |sql, params|
+        db.execute(sql, params)
+      end
+      expect(db.execute('SELECT a, b FROM target').to_a).to eq(
+        [
+          [3, 'Replication']
         ]
       )
     end
